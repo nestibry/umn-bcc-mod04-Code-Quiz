dbe883837924e5ec7efab649493633544db49f84
nestibry <nestibry@gmail.com>
2023-10-03 23:41:43 -0500

    Added three questions to arrQuestions...they are randomly picked...switched main in html index to just the start prompt

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,33 @@
 var buttonContainer = window.document.querySelector(".buttons");
+var promptContainer = window.document.querySelector(".prompt");
 
+
+// Look into whether do all strings or just 0/1's for isAnswer
 var arrQuestions = [
-    {id:0, prompt:"What is a best choice?", choices:[
-        {isAnswer:"no", text:"A"}, 
-        {isAnswer:"no", text:"B"}, 
-        {isAnswer:"yes", text:"C"},
-        {isAnswer:"no", text:"D"}
-    ]},
-    {id:1, prompt:"What is the best color?", choices:[
-        {isAnswer:"yes", text:"blue"}, 
-        {isAnswer:"no", text:"green"}, 
-        {isAnswer:"no", text:"red"},
-        {isAnswer:"no", text:"black"}
-    ]}
+    {id:0, prompt:`How could you use pseudo-elements to display the message, 'Thanks for checking the box!', after a user clicks on a check box?`, 
+        choices:[
+            {isAnswer:"1", text:`.checkbox:checked + label::after {content: "Thanks for checking the box!";}`}, 
+            {isAnswer:"0", text:`.checkbox:checked + label:: after {message:"Thanks for checking the box!";}`}, 
+            {isAnswer:"0", text:`.checkbox + label {message:"Thanks for checking the box!";}`},
+            {isAnswer:"0", text:`.checkbox + label {content:"Thanks for checking the box!";}`}
+        ]
+    },
+    {id:1, prompt:`What is wireframing?`, 
+        choices:[
+            {isAnswer:"1", text:`A blueprint of our website's page layout.`}, 
+            {isAnswer:"0", text:`A 3D model of our websites structure made from wires.`}, 
+            {isAnswer:"0", text:`A CSS library that helps in the creation of borders around our boxed elements.`},
+            {isAnswer:"0", text:`Wireframing helps us quickly set up our HTML page.`}
+        ]
+    },
+    {id:2, prompt:`How is Flexbox related to mobile-first design?`, 
+        choices:[
+            {isAnswer:"1", text:`The flex layout allows responsive elements within a container to be automatically arranged depending upon screen size (or device).`}, 
+            {isAnswer:"0", text:`The flex layout allows developers to design the mobile user interface first, then other screen resolutions and devices.`}, 
+            {isAnswer:"0", text:`The flex layout allows non-responsive elements within a container to behave as responsive elements depending upon screen size (or device).`},
+            {isAnswer:"0", text:`The flex layout allows for the user to select an interface based on the screen size (or device).`}
+        ]
+    },
 ];
 
 
@@ -20,33 +35,25 @@ var arrQuestions = [
 
 buttonContainer.addEventListener("click", function(event){
 
+    // Get the button pressed AND see if it is the correct answer
     var element = event.target;
-    console.log(element);
-
     var isAnswer= element.getAttribute("data-answer");
+    // console.log(`isAnswer: ${isAnswer}  type: ${typeof isAnswer}`);
 
-    if(isAnswer.toLowerCase() === "yes" || isAnswer.toLowerCase() === "y"){
+    // If Correct Answer: Add points
+    // If Wrong Answer: Subtract time
+    if(isAnswer.toLowerCase() === "yes" || isAnswer.toLowerCase() === "y" || isAnswer == 1){
 
-        console.log("Correct Answer");
-
-        var newElement= document.createElement("button");
-        newElement.setAttribute("data-answer", "no");
-        newElement.textContent = "This is a Test Button..."
-        buttonContainer.appendChild(newElement);
-
+        console.log("User Selected the Correct Answer");
 
     } else {
 
-        console.log("Wrong Answer");
-
+        console.log("User Selected the Wrong Answer");
     }
 
-  
-    console.log("Buttons Container:");
-    console.log(buttonContainer.children.length);
-
-
     // Remove the buttons to get ready for the new question's buttons
+    // console.log("Buttons Container:");
+    // console.log(buttonContainer.children.length);
     for(var j = (buttonContainer.children.length - 1); j >= 0; j--) {
         buttonContainer.children[j].remove();
     }
@@ -54,13 +61,19 @@ buttonContainer.addEventListener("click", function(event){
     // Choose a random question from the Array of Questions and add choice buttons to the page
     var newQuestion = arrQuestions[ Math.floor( Math.random() * arrQuestions.length ) ];
 
+    promptContainer.textContent = newQuestion.prompt;
+    console.log(`Prompt: ${newQuestion.prompt}`);
+
     for(var i=0; i < newQuestion.choices.length; i++) {
         var newElement= document.createElement("button");
         newElement.setAttribute("data-answer", newQuestion.choices[i].isAnswer);
         newElement.textContent = newQuestion.choices[i].text;
         buttonContainer.appendChild(newElement);
+        // Console log the correct answer --> Look into whether do all strings or just 0/1's for isAnswer
+        if(newQuestion.choices[i].isAnswer.toLowerCase() === "yes" || newQuestion.choices[i].isAnswer.toLowerCase() === "y" || newQuestion.choices[i].isAnswer == 1) {
+            console.log(`Correct Answer: \nIndex: ${i} \n${newQuestion.choices[i].text}`)
+        }
     }
-    
 });
 
 
